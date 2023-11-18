4e459e41ee5687f2a84d1b5eb4795af192f3aa34
ahuang28 <amyhuang0605@gmail.com>
2023-11-17 21:57:05 -0500

    added mongoose

--- api/src/app.js
+++ api/src/app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+require('dotenv').config();
 const app = express();
-
-app.use(cors());
-
-app.use(function (req, res, next) {
-    console.log('Time:', Date.now(), req.method, req.url);
-    next();
-})
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+mongoose.connect(process.env.MONGO, { retryWrites: true, w: 'majority'}).then(() => {
+    console.log('Connected to MongoDB');
+    server();
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+const server = () => {
+
+    app.use(cors());
+
+    app.use(function (req, res, next) {
+        console.log('Time:', Date.now(), req.method, req.url);
+        next();
+    })
+
+    app.get('/', (req, res) => {
+        res.send('Hello World');
+    });
+
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+
+}
