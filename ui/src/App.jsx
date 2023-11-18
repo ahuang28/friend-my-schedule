1b271c8238620761ac09ca80c685ad782324da6d
wen-wong <wen.wong@mail.mcgill.ca>
2023-11-18 10:56:02 -0500

    Setup tailwind nextui and react router

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes, Link } from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/chat">Chat</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/matches">Matches</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Hello World</div>} />
+        <Route path="/chat" element={<div>Chat</div>} />
+        <Route path="/profile" element={<div>Profile</div>} />
+        <Route path="/matches">
+          <Route index element={<div>Matches</div>} />
+        </Route>
+        <Route path="*" element={<div>404 Error Page</div>}/>
+      </Routes>
     </>
   )
 }
